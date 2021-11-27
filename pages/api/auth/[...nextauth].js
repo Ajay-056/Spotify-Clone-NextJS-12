6fcafd8a273bfc2d3ce1f82751203b6cb63af8f6
ak-the-dev <balaajay19@gmail.com>
2021-11-28 01:44:16 +0530

    50% Login Page & Next Auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,74 @@
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import SpotifyAPI, { LOGIN_URL } from '../../../lib/spotify';
+
+async function refreshAccessToken(token) {
+  try {
+    SpotifyAPI.setAccessToken(token.accessToken);
+    SpotifyAPI.setAccessToken(token.refreshToken);
+
+    const { body: refreshedToken } = await SpotifyAPI.refreshAccessToken();
+
+    console.log('Refreshed token is', refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Initil Sign In
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      // Return previous token if the access token hasn't expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('Token in valid ...');
+        return token;
+      }
+
+      // Access Token expired, so refresh it...token
+      console.log('Token expired, refreshing...');
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
+});
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,92 @@
-function Login() {
+import { ChevronDoubleRightIcon } from '@heroicons/react/outline';
+import { getProviders, signIn } from 'next-auth/react';
+import Image from 'next/image';
+
+function Login({ providers }) {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div
+      className="text-center h-screen w-screen flex items-center justify-center relative"
+      style={{ backgroundColor: 'whitesmoke' }}
+    >
+      <div className="bg-black w-screen h-40 flex items-center justify-center relative">
+        <img
+          className="w-16 md-20 lg:w-24 bg-white rounded-full mr-7 md:mr-12 "
+          src="https://links.papareact.com/9xl"
+          alt="spotify logo"
+        />
+        <h1
+          className="text-4xl lg:text-9xl md:text-7xl  uppercase font-bold"
+          style={{
+            color: '#1ed760',
+          }}
+        >
+          spotify
+        </h1>
+        <small className="absolute top-[6.5rem] right-[24rem] font-extrabold uppercase text-lg text-white hidden lg:block">
+          Clone
+        </small>
+
+        {Object.values(providers).map((provider) => (
+          <div
+            key={provider.name}
+            className="absolute top-auto w-20 h-20 bg-transparent right-10 cursor-pointer"
+          >
+            <ChevronDoubleRightIcon
+              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+              className="w-20 h-20 animate-pulse"
+              style={{ color: '#1ed760' }}
+            />
+          </div>
+        ))}
+      </div>
+      <footer
+        className="absolute bottom-0 w-96 h-16 bg-gray-300 rounded-t-md"
+        style={{
+          textAlign: 'center',
+          display: 'flex',
+          marginTop: '1rem',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+        }}
+      >
+        <a
+          href="https://github.com/Ajay-056/Whatsapp-Clone-NextJS"
+          target="_blank noreferrer"
+        >
+          <Image src="/icons8-github.svg" alt="github" height={40} width={40} />
+        </a>
+        <a href="https://www.twitter.com/balaajay19" target="_blank noreferrer">
+          <Image
+            src="/icons8-twitter-48.png"
+            alt="twitter"
+            height={40}
+            width={40}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ajay-krishna-065a1a162"
+          target="_blank noreferrer"
+        >
+          <Image
+            src="/icons8-linkedin-48.png"
+            alt="linkedin"
+            height={40}
+            width={40}
+          />
+        </a>
+      </footer>
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
