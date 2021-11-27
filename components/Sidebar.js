17170de7ee345cf8e2bc56f9ab46f6bda2ae08c7
ak-the-dev <balaajay19@gmail.com>
2021-11-27 15:06:57 +0530

    skeleton : Half of sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,47 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from '@heroicons/react/outline';
+
 function Sidebar() {
-  return <div>I am a sidebar;</div>;
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>create Playlist</p>
+        </button>    
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your Episodes</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
+
